348116634887d866a1ea32f87264b43d2bca4ecf
hiyokiki <bada8317@gmail.com>
2025-05-05 20:03:49 +0900

    carrer 배경이미지

--- js/main.js
+++ js/main.js
@@ -9,16 +9,51 @@
 
 
 /* jinuk */
-
-
 // carrer
+const stories = [
+  document.getElementById('story1'),
+  document.getElementById('story2'),
+  document.getElementById('story3')
+];
+
+const leftBtn = document.querySelector('.left');
+const rightBtn = document.querySelector('.right');
+const thumbs = document.querySelectorAll('.story_thumbs a');
+
+let currentIndex = 1; 
+
+function active(index) {
+  stories.forEach(story => story.classList.remove('active'));
+  stories[index].classList.add('active');
+  currentIndex = index;
+}
+
+rightBtn.addEventListener('click', function () {
+  const nextIndex = (currentIndex + 1) % stories.length;
+  active(nextIndex);
+});
+
+leftBtn.addEventListener('click', function () {
+  const prevIndex = (currentIndex - 1 + stories.length) % stories.length;
+  active(prevIndex);
+});
+
+
+
 
 
   
+
+
+
+
+
 // top버튼
-document.querySelector('.top button').onclick = function () {
+const topButton = document.querySelector('.top button');
+
+topButton.addEventListener('click', function () {
   window.scrollTo({ top: 0, behavior: 'smooth' });
-};
+});
 // //top 버튼
 
 
@@ -26,10 +61,10 @@ document.querySelector('.top button').onclick = function () {
 
 
 const icons = [
-    { id: 'face', normal: 'images/facebook.svg', hover: 'images/facebook_hover.svg' },
+    { id: 'face', normal:'images/facebook.svg', hover:'images/facebook_hover.svg' },
     { id: 'insta', normal: 'images/instagram.svg', hover: 'images/insta_hover.svg' },
-    { id: 'twitter', normal: 'images/twitter.svg', hover: 'images/twitter_hover.svg' },
-    { id: 'youtube', normal: 'images/youtube.svg', hover: 'images/youtube_hover.svg' }
+    { id: 'twitter', normal:'images/twitter.svg', hover:'images/twitter_hover.svg' },
+    { id: 'youtube', normal:'images/youtube.svg', hover:'images/youtube_hover.svg' }
   ];
 
   icons.forEach(icon => {
